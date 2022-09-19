77c91ba9215d339c724ebbc4b27072e39edb3496
Byron Machin <bmachin7@gmail.com>
2022-09-18 20:09:53 -0500

    Creation of TaskContext

---
+++ react-tasks-application/src/context/TaskContext.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect, createContext } from "react";
+import { tasks as data } from "../data/tasks";
+
+export const TaskContext = createContext();
+
+function TaskContextProvider(props) {
+  const [tasks, setTasks] = useState([]);
+
+  function createTask(task) {
+    setTasks([
+      ...tasks,
+      {
+        title: task.title,
+        id: tasks.length,
+        description: task.description,
+      },
+    ]);
+  }
+
+  function deleteTask(taskId) {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  }
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  return (
+    <TaskContext.Provider
+      value={{
+        tasks,
+        createTask,
+        deleteTask,
+      }}
+    >
+      {props.children}
+    </TaskContext.Provider>
+  );
+}
+
+export default TaskContextProvider;
